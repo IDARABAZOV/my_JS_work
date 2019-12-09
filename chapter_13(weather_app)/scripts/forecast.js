09091dab86d52001ed852b17da8af116e137aa2f
IDARABAZOV <idar112@gmail.com>
2019-12-09 18:39:42 +0300

    first show

--- chapter_13(weather_app)/scripts/forecast.js
+++ chapter_13(weather_app)/scripts/forecast.js
@@ -1,1 +1,34 @@
 const key = 'HDK99oJtAqdcLG7XGZqgDoXy95laccL4';
+
+// Get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = response.json();
+
+    return data;
+
+};
+
+// Get city information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+// getCity('manchester').then(data => {
+//     return getWeather(data.Key);
+// }).then(data => {
+//     console.log(data)
+// }).catch(err => console.log(err));
+
