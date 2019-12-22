902e4bcfc8f59a23a6b55b25d3c45b60b7d526e2
IDAR <idar112@gmail.com>
2019-12-23 02:17:22 +0300

    Webpack

--- chapter_19/src/index.js
+++ chapter_19/src/index.js
@@ -1,13 +1,1 @@
-const greet = name => {
-    console.log(`hello ${name}`);
-};
-
-greet('mario');
-greet('luigi');
-greet('link');
-
-class User {
-    constructor(){
-        this.score = 0;
-    }
-}
+console.log(1);
